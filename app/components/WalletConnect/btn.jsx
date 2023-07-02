f2334e301d93680e2b3af629eac87d560bb24a44
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-07-03 00:06:49 +0530

    changes

--- app/components/WalletConnect/btn.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const btn = () => {
-  return (
-   <button> Hey </button>
-  )
-}
-
-export default btn
---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+<div className="font-bold text-3xl">Popular Collections </div>
+
+    </main>
+  )
+}
