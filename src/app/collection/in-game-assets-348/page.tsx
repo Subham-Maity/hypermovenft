32e072aa28a19215dd7f5124223e83a8e4fa2468
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-07-06 15:23:25 +0530

    Auction and In Game

--- src/app/collection/in-game-assets-348/page.tsx
+++ src/app/collection/in-game-assets-348/page.tsx
@@ -1,9 +1,22 @@
+"use client"
 import React from 'react';
+import BannerCard from "@/app/components/BannerCard";
+import Onsale from "@/app/collection/in-game-assets-348/OnSale/Onsale";
+import ConsumablesHead from "@/app/collection/in-game-assets-348/Heading/page";
 
 const Page = () => {
     return (
-        <div>
-            No
+        <div >
+            <ConsumablesHead/>
+           <div className="mt-10">
+            <BannerCard
+                img="https://fabweltmarketplace.s3.us-east-2.amazonaws.com/2023/0/27/collectionBanner/IngameassetsBanner.png"
+                href="https://market.fabwelt.com/collection/consumables-164"
+                TabText={['On Sale', 'On Auction']}
+                Child={[<Onsale key={0}/>]}
+            />
+           </div>
+            <div className="mb-16"></div>
         </div>
     );
 };
