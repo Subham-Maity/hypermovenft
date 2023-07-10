ebffbcf4b0921932c137294d45c9e3ed9826b378
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-07-10 15:56:15 +0530

    card column problem fix

--- src/app/components/LargeCard.tsx
+++ src/app/components/LargeCard.tsx
@@ -8,13 +8,14 @@ interface LargeCardProps {
     createdBy?: string;
 }
 
-const LargeCard: React.FC<LargeCardProps> = ({ banner, images, text, createdBy }) => {
+const LargeCard: React.FC<LargeCardProps> = ({banner, images, text, createdBy}) => {
     return (
-        <div className="w-full mt-2 bg-[#343444] rounded-2xl m-5">
+        <div className=" w-96 flex flex-col bg-[#343444] rounded-2xl overflow-hidden ">
+
             {banner && (
                 <div className="flex justify-center">
                     <Image
-                        className="mt-2 rounded-2xl w-full p-6"
+                        className="mt-1 rounded-2xl w-full p-3 object-cover"
                         src={banner}
                         height={600}
                         width={300}
@@ -24,16 +25,12 @@ const LargeCard: React.FC<LargeCardProps> = ({ banner, images, text, createdBy }
             )}
 
             {images && images.length > 0 && (
-                <div className="p-2 mt-2 flex justify-center gap-4">
+                <div className="p-2 mt-2 flex justify-center">
                     {images.map((image, index) => (
-                        <div key={index} className="flex justify-center">
+                        <div key={index} >
                             <Image
-                                className="rounded-[25px] gap-4 p-2 sm:w-[150px] md:w-[200px] lg:w-[300px]"
-                                src={image.image}
-                                height={600}
-                                width={300}
-                                alt=""
-                            />
+                                className="rounded-[25px] gap-4 p-1 sm:w-[150px] md:w-[200px] lg:w-[400px] "
+                                src={image.image} height={600} width={600} alt=""/>
                         </div>
                     ))}
                 </div>
@@ -42,6 +39,7 @@ const LargeCard: React.FC<LargeCardProps> = ({ banner, images, text, createdBy }
             {text && <div className="text-white mt-4 p-2">{text}</div>}
 
             {createdBy && <div className="text-white mt-2 p-2">Created by {createdBy}</div>}
+
         </div>
     );
 };
--- src/pages/home/Features.tsx
+++ src/pages/home/Features.tsx
@@ -4,7 +4,7 @@ import LargeCard from '@/app/components/LargeCard';
 import Link from 'next/link';
 
 const Features: React.FC = () => {
-    const [sampleMap] = useState<number>(3);
+    const [sampleMap] = useState<number>(2);
 
     interface Image {
         image: string;
@@ -20,7 +20,7 @@ const Features: React.FC = () => {
 
 
     return (
-        <section className="flex flex-col gap-2 justify-center items-center min-h-screen h-auto bg-features-custom">
+        <section className=" w-full p-8 bg-features-custom">
             <div className="flex justify-between px-4 md:px-16 items-center w-full">
                 <div className="text-4xl text-amber-50 font-bold mb-4">Popular Collections</div>
                 <Link href="/">
@@ -28,34 +28,55 @@ const Features: React.FC = () => {
                 </Link>
             </div>
 
-            <div className="flex gap-2 items-center justify-center flex-wrap sm:flex-nowrap w-full px-4 md:px-16">
-                {Array(sampleMap)
-                    .fill(0)
-                    .map((_, i) => (
-                        <LargeCard
-                            key={i}
-                            banner="https://fabweltmarketplace.s3.us-east-2.amazonaws.com/2023/0/27/collectionBanner/85e40484-a3fa-4f02-a655-5c9c156160e3"
-                            images={[
-                                {
-                                    image:
-                                        'https://fabweltmarketplace.s3.us-east-2.amazonaws.com/2022/9/27/nft/health.webp',
-                                    // href: '/link1', // use href instead of link
-                                },
-                                {
-                                    image:
-                                        'https://fabweltmarketplace.s3.us-east-2.amazonaws.com/2022/9/27/nft/speed.webp',
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center justify-center max-w-screen-xl mx-auto">
+                <LargeCard
 
-                                },
-                                {
-                                    image:
-                                        'https://fabweltmarketplace.s3.us-east-2.amazonaws.com/2022/9/27/nft/speed.webp',
-                                },
-                            ]}
-                            text="Sample Text"
-                            createdBy="abc"
-                        />
-                    ))}
+                    banner="https://fabweltmarketplace.s3.us-east-2.amazonaws.com/2023/0/27/collectionBanner/85e40484-a3fa-4f02-a655-5c9c156160e3"
+                    images={[
+                        {
+                            image:
+                                'https://fabweltmarketplace.s3.us-east-2.amazonaws.com/2022/9/27/nft/health.webp',
+                            // href: '/link1', // use href instead of link
+                        },
+                        {
+                            image:
+                                'https://fabweltmarketplace.s3.us-east-2.amazonaws.com/2022/9/27/nft/speed.webp',
+
+                        },
+                        {
+                            image:
+                                'https://fabweltmarketplace.s3.us-east-2.amazonaws.com/2022/9/27/nft/speed.webp',
+                        },
+                    ]}
+                    text="Sample Text"
+                    createdBy="abc"
+                />
+                <LargeCard
+
+                    banner="https://fabweltmarketplace.s3.us-east-2.amazonaws.com/2022/8/22/collectionBanner/banner-collection.png"
+                    images={[
+                        {
+                            image:
+                                'https://fabweltmarketplace.s3.us-east-2.amazonaws.com/2022/9/27/nft/health.webp',
+                            // href: '/link1', // use href instead of link
+                        },
+                        {
+                            image:
+                                'https://fabweltmarketplace.s3.us-east-2.amazonaws.com/2022/9/27/nft/speed.webp',
+
+                        },
+                        {
+                            image:
+                                'https://fabweltmarketplace.s3.us-east-2.amazonaws.com/2022/9/27/nft/speed.webp',
+                        },
+                    ]}
+                    text="Sample Text"
+                    createdBy="abc"
+                />
+
             </div>
+
+
         </section>
     );
 };
