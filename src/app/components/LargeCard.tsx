e7cb4af337c4aa687d3f606ad16b3eec9c140431
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-07-10 18:32:41 +0530

    responsiveness

--- src/app/components/LargeCard.tsx
+++ src/app/components/LargeCard.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
 
 interface LargeCardProps {
     banner?: string;
@@ -8,10 +8,11 @@ interface LargeCardProps {
     createdBy?: string;
 }
 
-const LargeCard: React.FC<LargeCardProps> = ({ banner, images, text, createdBy }) => {
+const LargeCard: React.FC<LargeCardProps> = ({banner, images, text, createdBy}) => {
     return (
         <div className="">
-            <div className="w-full flex flex-col bg-[#343444] rounded-2xl overflow-hidden">
+            <div className=" w-full flex flex-col bg-[#343444] rounded-2xl overflow-hidden ">
+
                 {banner && (
                     <div className="flex justify-center">
                         <Image
@@ -27,15 +28,10 @@ const LargeCard: React.FC<LargeCardProps> = ({ banner, images, text, createdBy }
                 {images && images.length > 0 && (
                     <div className="p-2 mt-2 flex justify-center">
                         {images.map((image, index) => (
-                            <div key={index} className="sm:w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
+                            <div key={index} >
                                 <Image
-                                    className="rounded-[25px] gap-4 p-1 w-full"
-                                    src={image.image}
-                                    layout="responsive"
-                                    height={600}
-                                    width={600}
-                                    alt=""
-                                />
+                                    className="rounded-[25px] gap-4 p-1 sm:w-[150px] md:w-[200px] lg:w-[400px] "
+                                    src={image.image} height={600} width={600} alt=""/>
                             </div>
                         ))}
                     </div>
@@ -44,6 +40,7 @@ const LargeCard: React.FC<LargeCardProps> = ({ banner, images, text, createdBy }
                 {text && <div className="text-white mt-4 p-2">{text}</div>}
 
                 {createdBy && <div className="text-white mt-2 p-2">Created by {createdBy}</div>}
+
             </div>
         </div>
     );
